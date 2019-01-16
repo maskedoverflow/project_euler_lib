460189e56a00d19dea7a3d880c81870c92550a59
Benjamin Simon <git.besim@gmail.com>
2019-01-16 12:03:51 +0100

    working on test functions

--- fibonacci_test.c
+++ fibonacci_test.c
@@ -1,6 +1,10 @@
 #include "generators.h"
+#include <stdio.h>
 
 int main(void) {
+  for (int i = 1; i < 5; i++){
+    printf("Fibnoacci no.%i: %li\n", i, fibonacci_gen(0));
+  }
 
-  return -1;
+  return 0;
 }
