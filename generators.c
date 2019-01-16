ac9aae06d261b26368428bda30e4aa7a830ce5b8
Benjamin Simon <git.besim@gmail.com>
2019-01-16 12:03:45 +0100

    modifying generators

--- generators.c
+++ generators.c
@@ -2,15 +2,15 @@
 
 int fibonacci_gen(int reset)
 {
-  static unsigned long first = 1;
+  static unsigned long first = 0;
   static unsigned long second = 1;
 
   if (reset) {
-    first = 1;
+    first = 0;
     second = 1;
   }
 
   unsigned long temp = first + second;
-  second = first;
+  first = second;
   return first = temp;      
 }
