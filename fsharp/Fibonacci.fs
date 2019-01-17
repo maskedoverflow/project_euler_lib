b8ff174496801fadf0b1f77d4eb4c5eedb5d214f
Kamillo Ferry <git.hugh@gmail.com>
2019-01-17 23:41:38 +0100

    Tail-recursive fibonacci with a little demonstration

--- fsharp/Fibonacci.fs
+++ fsharp/Fibonacci.fs
@@ -1,13 +1,18 @@
 namespace ProjectEulerLib
 
 module Fibonacci =
-  
-  // A tail recursive version of Fibonacci
-  let FibRec n = 
-    let rec FibTail (x:bigint) (y:bigint) = function
-      | 0 -> y
-      | n -> FibTail y (bigint.Add(x,y)) (n-1)
-      
-    FibTail 0I 1I n
 
-    // Testing code follows due, for now it's easier to copy that stuff into fsi
+  // A tail recursive version of Fibonacci
+  let fibRec n = 
+    let rec fibTail (x:bigint) (y:bigint) = function
+      | 0 -> y
+      | n -> fibTail y (bigint.Add(x,y)) (n-1)
+      
+    fibTail 0I 1I n
+
+
+module TestFibonacci =
+  let fibRec_Test = 
+    let fibs = Set.map Fibonacci.fibRec (set [1..10])
+    Seq.iter (fun x -> printfn "%A" x) fibs
+    
